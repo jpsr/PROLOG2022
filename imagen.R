d41ca9b59100a8c71a4070610bc17caa0147e938
Juan Pablo Serrano-Rubio <juanpablo.serranorubio@gmail.com>
2022-03-01 15:38:05 -0600

    Update imagen.R

--- imagen.R
+++ imagen.R
@@ -4,6 +4,10 @@
 library(magick)
 library(rsvg)
 
-tiger <- image_read_svg('http://jeroen.github.io/images/tiger.svg', width = 350)
-plot(tiger)
-print(tiger)
+#tiger <- image_read_svg('http://jeroen.github.io/images/tiger.svg', width = 350)
+#plot(tiger)
+#print(tiger)
+
+
+frink <- image_read("https://jeroen.github.io/images/frink.png")
+plot(frink)

43da183078169c56371a43f89673239b52c41801
jpsr <juanpablo.serranorubio@gmail.com>
2022-03-03 13:25:48 -0600

    030320222

--- imagen.R
+++ imagen.R
@@ -8,7 +8,8 @@ library(rsvg)
 
 #tiger <- image_read_svg('http://jeroen.github.io/images/tiger.svg', width = 350)
 #plot(tiger)
-#print(tiger)
+
+fimage <- image_read("")
+print(fimage)
 
 
-im <- image_read("https://github.com/jpsr/PROLOG2022/blob/37f3859c8d8c8b22598cd4520032e32e7e9ba88b/img.jpg")
